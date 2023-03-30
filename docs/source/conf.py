9ba144a44e7aabf32cbf0337a95b018d9d6805eb
Joel Chacón Castillo <joel.chacon@cimat.mx>
2023-03-30 12:23:47 -0600

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'OSPO_HMS_Fire'
+copyright = '2022, Joel'
+author = 'Joel'
 
 release = '0.1'
 version = '0.1.0'
